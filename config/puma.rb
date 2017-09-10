1e82d67e66dade11726e9202a11c72e2366408db
Sudara <sudara@alonetone.com>
2017-09-10 03:02:29 +0200

    default env is production in puma.rb

--- config/puma.rb
+++ config/puma.rb
@@ -1,7 +1,7 @@
 threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
 threads threads_count, threads_count
 
-env = ENV.fetch("RAILS_ENV") { "development" }
+env = ENV.fetch("RAILS_ENV") { "production" }
 environment env
 if env == "production"
   workers 2
