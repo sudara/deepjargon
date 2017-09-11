316707f46a45930936a8b8ef228933b9f7e9eb7b
Sudara <sudara@alonetone.com>
2017-09-11 03:52:26 +0200

    Cache index.html and remove cache on deploy

--- Gemfile
+++ Gemfile
@@ -14,13 +14,6 @@ gem 'uglifier', '>= 1.3.0'
 gem 'kramdown'
 gem 'katex'
 gem 'jbuilder', '~> 2.5'
-# Use Redis adapter to run Action Cable in production
-# gem 'redis', '~> 3.0'
-# Use ActiveModel has_secure_password
-# gem 'bcrypt', '~> 3.1.7'
-
-# Use Capistrano for deployment
-# gem 'capistrano-rails', group: :development
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,6 +1,6 @@
 ActiveSupport.escape_html_entities_in_json = false
 class SiteController < ApplicationController
-
+  after_action :render_response_to_file, only: :index
 
   def index
     # Gather each markdown file
@@ -21,6 +21,10 @@ class SiteController < ApplicationController
   end
 
   protected
+
+  def render_response_to_file
+    File.open(File.join(Rails.root,'public/index.html'), "wb+") { |f| f.write(response.body) }
+  end
 
   def convert_to_html(markdown)
     Kramdown::Document.new(markdown, input: 'GFM', math_engine: 'katex').to_html
